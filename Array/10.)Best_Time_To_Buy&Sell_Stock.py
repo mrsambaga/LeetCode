116f034f059cb8dad83a2ab3c79b70df32b5c160
mrsambaga <98897219+mrsambaga@users.noreply.github.com>
2022-05-01 13:21:47 +0700

    Create 10.)Best_Time_To_Buy&Sell_Stock.py

---
+++ Array/10.)Best_Time_To_Buy&Sell_Stock.py
@@ -0,0 +1,30 @@
+"""
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+Example :
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+"""
+
+class Solution(object):
+    def maxProfit(self, prices):
+        high = 0
+        buy = prices[0]
+        profit = 0
+        for x in range(1, len(prices)):
+            if prices[x] < buy :
+                buy = prices[x]
+            profit = prices[x] - buy
+            if profit > high :
+                high = profit
+        return high
+
+"""
+We can solve this problem using linear iteration. Basically, we can keep the price when we buy the stock outside iteration and update it whenever we see a stock
+cheeper than the buy price. We can just start the iteration from the 2nd element as the sell price and use 1st element as the buy price and then subtracting 
+the sell price with the buy price. 
+"""
