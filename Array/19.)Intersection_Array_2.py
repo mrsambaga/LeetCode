e3f07dae106dfa2bc31c4654531f6b86071bf086
mrsambaga <98897219+mrsambaga@users.noreply.github.com>
2022-05-03 23:44:01 +0700

    Create 19.)Intersection_Array_2.py

---
+++ Array/19.)Intersection_Array_2.py
@@ -0,0 +1,20 @@
+"""
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+Example 1:
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+"""
+
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        lst = []
+        for x in nums1:
+            if x in nums2 and x not in lst:
+                lst += [x] * min(nums1.count(x), nums2.count(x))
+        return lst
+      
+"""
+For this problem, we can store the element in another list. We can find out how many time the element appear in both arrays with count() and find the min of those 2 
+numbers to make sure the elements appear that many times in both array. Then, we can append it to the list.
+"""
