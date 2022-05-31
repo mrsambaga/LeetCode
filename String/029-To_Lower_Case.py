c3ba9d1df9cac7d4ac9df6aa6488d0cd0b62f270
mrsambaga <98897219+mrsambaga@users.noreply.github.com>
2022-05-31 19:59:50 +0700

    Create 029-To_Lower_Case.py

---
+++ String/029-To_Lower_Case.py
@@ -0,0 +1,15 @@
+"""
+Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+Example :
+Input: s = "Hello"
+Output: "hello"
+"""
+
+class Solution(object):
+    def toLowerCase(self, s):
+        return s.lower()
+      
+"""
+This problem can be solved easily in Python
+"""
