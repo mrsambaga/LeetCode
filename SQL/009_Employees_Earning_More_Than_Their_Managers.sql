6423aa8fa7abe94d986b8079e6de8e049f110df3
mrsambaga <98897219+mrsambaga@users.noreply.github.com>
2022-07-26 22:46:45 +0700

    Create 009_Employees_Earning_More_Than_Their_Managers.sql

---
+++ SQL/009_Employees_Earning_More_Than_Their_Managers.sql
@@ -0,0 +1,28 @@
+/* Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| salary      | int     |
+| managerId   | int     |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the ID of an employee, their name, salary, and the ID of their manager.
+
+Write an SQL query to find the employees who earn more than their managers.
+Return the result table in any order.
+The query result format is in the following example. */
+
+SELECT A.name AS Employee
+FROM Employee A
+INNER JOIN (
+    SELECT *
+    FROM Employee
+    WHERE managerId IS NULL
+) manager ON A.managerId = manager.id
+WHERE A.salary > manager.salary
+
+/* For this problem we can use inner join to join table 1 with employee only and table 2 with manager only. 
+Then we can filter the employe salary to be greater than manager salary */
