2a8d5e8e5e8c33359f6baab88a7dbabf55b68a02
M. Radjasa Sambaga <98897219+mrsambaga@users.noreply.github.com>
2023-05-12 11:24:04 +0700

    Create SearchInsertPosition.go

---
+++ Array/004-SearchInsertPosition/SearchInsertPosition.go
@@ -0,0 +1,10 @@
+func searchInsert(nums []int, target int) int {
+    for i:=0;i<len(nums);i++ {
+        if nums[i] >= target {
+            return i
+        } else if i == len(nums)-1 {
+            return i+1
+        }
+    }
+    return 0
+}
