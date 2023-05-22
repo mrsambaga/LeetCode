8e3984e7343610f43b3873500af3c6b37d450457
M. Radjasa Sambaga <98897219+mrsambaga@users.noreply.github.com>
2023-05-22 09:56:06 +0700

    Create SingleNumber.py

---
+++ Array/011-SingleNumber/SingleNumber.py
@@ -0,0 +1,35 @@
+"""
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+Example :
+Input: nums = [2,2,1]
+Output: 1
+"""
+
+class Solution(object):
+    def singleNumber(self, nums):
+        lst = []
+        for i in nums:
+            if i not in lst:
+                lst.append(i)
+            elif i in lst:
+                lst.remove(i)
+        return lst[0]
+      
+      
+class Solution(object):
+    def singleNumber(self, nums):
+        numMap = {}
+    
+        for num in nums:
+            if num in numMap:
+                numMap.pop(num)
+            else:
+                numMap[num] = num
+        
+        return numMap.keys()[0]
+
+"""
+For this problem we can use hash method where we store the element that we encounter and remove it if we find it again in the nums list after we iterate.
+"""
