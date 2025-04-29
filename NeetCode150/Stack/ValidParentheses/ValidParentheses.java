b8113dd33a1e293553562d693a1224d5da04f0d8
mrsambaga <m.r.sambaga@gmail.com>
2025-04-29 21:20:39 +0700

    add description for solution in ValidParentheses

--- NeetCode150/Stack/ValidParentheses/ValidParentheses.java
+++ NeetCode150/Stack/ValidParentheses/ValidParentheses.java
@@ -31,3 +31,9 @@ class Solution {
     }
 }
 
+// In this problem we can achieve optimal solution by using stack data structure using Deque Array.
+// Basically, we can iterate throught the string and add the char to the stack if its an open parentheseses.
+// If its a close parentheses, you can check with first element of the stack (lattest added). If its a matching type, remove it.
+// The closing parentheses ALWAYS has to match with the latest added open parentheses. In the end, the stack will be empty which mean its a valid parentheses.
+// This solution has O(n) time complexity (all the ltter in string) and O(n) space complexity (for storing open parentheses)
+
