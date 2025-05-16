edad828b88984ea4bffdf3d7a6ec190badc0b94d
mrsambaga <m.r.sambaga@gmail.com>
2025-05-16 20:08:18 +0700

    create solution for twosumII in java

---
+++ NeetCode150/TwoPointers/TwoSumII/TwoSumII.java
@@ -0,0 +1,24 @@
+package NeetCode150.TwoPointers.TwoSumII;
+
+public class TwoSumII {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[] {left + 1, right + 1};
+            }
+            if (sum > target) {
+                right--;
+            }
+            if (sum < target) {
+                left++;
+            }
+        }
+
+        return new int[] {-1, -1};
+    }
+}
+
